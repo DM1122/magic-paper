51c224fb40de7821af9c3b946f76764ff7ec4aea
DM1122 <34867698+DM1122@users.noreply.github.com>
2022-11-03 11:37:32 -0400

    Change poetry reference to absolute path

--- magic_paper/scripts/launch.sh
+++ magic_paper/scripts/launch.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 echo "Running launch script"
+
+echo "Current directory: $PWD. Changing directory..."
 cd /home/pi/magic-paper
-echo "Changed directory"
+echo "Changed directory to: $PWD"
+
+echo "Pulling git..."
 git pull
-echo "Git pulled"
-poetry run python magic_paper/main.py
-echo "Ran Poetry"
+
+echo "Running python script..."
+/home/pi/.local/bin/poetry run python magic_paper/main.py
+
+echo "Done!"
